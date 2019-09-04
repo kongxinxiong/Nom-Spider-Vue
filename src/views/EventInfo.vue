05163ff917072fd4049984b166da2df5d22d9e40
wudan <wudan_13091315@163.com>
2019-09-04 19:18:58 +0800

    activity

--- src/views/EventInfo.vue
+++ src/views/EventInfo.vue
@@ -10,9 +10,10 @@
           <div class="md-layout md-gutter padding">
             <div class="md-layout-item md-size-40 md-small-size-100 ml-auto">
               <div class="avatar">
-                <img : :src="
+                <img  :src="
                         'http://localhost:8080/api/event/image/' + event.photoURL
-                      " class="img-raised rounded img-fluid" />
+                      "
+                      class="img-raised rounded img-fluid" />
               </div>
             </div>
             <div class="md-layout-item md-size-40 md-small-size-100 mr-auto">
@@ -22,15 +23,16 @@
                   <span class="tim-note">{{ event.title }}</span>
                 </p>
                 <h4 class="title"><span class="tim-note">Description</span></h4>
-                  <p>
-                    <span class="tim-note">{{ event.description }}</span>
-                  </p>
+                <p>
+                  <span class="tim-note">{{ event.description }}</span>
+                </p>
 
                 <h4 class="title"><span class="tim-note">Number</span></h4>
                 <p>
                   <span class="tim-note">{{ event.maxnumber }}</span>
                 </p>
-                <h4 class="title"><span class="tim-note">Time &nbsp; & &nbsp; Location</span>
+                <h4 class="title">
+                  <span class="tim-note">Time &nbsp; & &nbsp; Location</span>
                 </h4>
                 <p>
                   <span class="tim-note"
@@ -40,28 +42,41 @@
                 </p>
               </div>
             </div>
-            </div>
+          </div>
           <div class="md-layout">
             <div class="md-layout-item md-size-40 md-small-size-100 ml-auto">
               <h4 class="title"><span class="tim-note">Enrollment</span></h4>
               <p>
-                <span class="tim-note">{{ event.description }}</span>
+                <span class="tim-note">{{ enrollment }}</span>
               </p>
             </div>
             <div
-                    class="md-layout-item md-size-40 md-small-size-100 mr-auto"
+              class="md-layout-item md-size-40 md-small-size-100 mr-auto"
             ></div>
           </div>
           <div class="md-layout md-gutter">
             <div class="md-layout-item " style="justify-content: center">
-              <md-button class="md-success" v-if="!isFinished" @click="participant">Enroll</md-button>
+              <md-button
+                class="md-success"
+                v-if="!isFinished"
+                @click="participant"
+                >Enroll</md-button
+              >
               <md-button v-else>Finished</md-button>
             </div>
             <div class="md-layout-item" style="justify-content: center">
-              <md-button class="md-just-icon" v-if="!isLiked" @click="clickLike">
+              <md-button
+                class="md-just-icon"
+                v-if="!isLiked"
+                @click="clickLike"
+              >
                 <md-icon>favorite</md-icon>
               </md-button>
-              <md-button class="md-just-icon md-rose" v-if="isLiked" @click="clickLike">
+              <md-button
+                class="md-just-icon md-rose"
+                v-if="isLiked"
+                @click="clickLike"
+              >
                 <md-icon>favorite</md-icon>
               </md-button>
             </div>
@@ -80,16 +95,16 @@
                   <div class="md-layout-item mx-auto md-small-size-100">
                     <h4 class="title"><span class="tim-note">Moment</span></h4>
                     <el-upload
-                            :show-file-list="false"
-                            accept=".png,jpg,jpeg"
-                            with-credentials
-                            name="file"
-                            :action="uploadImgUrl()"
-                            :on-error="uploadImgError"
-                            :on-success="uploadImgSuccess"
-                            :before-upload="beforImgUpload"
-                            enctype="multipart/form-data"
-                            :file-list="carousel"
+                      :show-file-list="false"
+                      accept=".png,jpg,jpeg"
+                      with-credentials
+                      name="file"
+                      :action="uploadImgUrl()"
+                      :on-error="uploadImgError"
+                      :on-success="uploadImgSuccess"
+                      :before-upload="beforImgUpload"
+                      enctype="multipart/form-data"
+                      :file-list="carousel"
                     >
                       <md-button class="md-success md-sm">Upload</md-button>
                       <!--                <div slot="tip" class="el-upload__tip">You can only upload required picture format</div>-->
@@ -97,12 +112,15 @@
                     <p>
                       <br />
                       <span class="tim-note"
-                      >Share you Moment within the Activity</span
+                        >Share you Moment within the Activity</span
                       >
                     </p>
                     <md-card>
                       <el-carousel trigger="click" :interval="5000">
-                        <el-carousel-item v-for="item in carousel" :key="item.image">
+                        <el-carousel-item
+                          v-for="item in carousel"
+                          :key="item.image"
+                        >
                           <h4 class="carousel-caption">
                             <md-icon>location_on</md-icon>
                             {{ item.title }}
@@ -119,15 +137,14 @@
           </md-card>
         </div>
       </div>
-      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import requestAPI from "../plugins/request";
 import { createRequest, sendFormRequest } from "../plugins/request";
 import { setTimeout } from "timers";
-
 
 export default {
   name: "ActivityInfo",
@@ -161,40 +178,57 @@ export default {
   },
   created: function() {
     this.eventid = this.$route.params.eventid;
+    localStorage.setItem(
+            "Authorization",
+            "{\n" +
+            '"id": 25,\n' +
+            '"name": "mark",\n' +
+            '"birthday": "2019-08-26T09:26:57.000+0000",\n' +
+            '"location": null,\n' +
+            '"username": null,\n' +
+            '"password": null,\n' +
+            '"email": null,\n' +
+            '"photoURL": null\n' +
+            "}"
+    );
+    this.userInfo = JSON.parse(localStorage.getItem("Authorization"));
 
     //request to get all the detailed information of an event
-    requestAPI({ url: "http://localhost:8080/api/event/" + this.eventid ,
+    requestAPI({
+      url: "http://localhost:8080/api/event/" + this.eventid,
       method: "GET",
-      headers:{
-        'Content-Type':'application/json'
-      }})
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
       .then(res => {
-        this.event = JSON.parse(res);
+        this.event = JSON.parse(JSON.stringify(res)).data;
       })
       .catch(err => {
         alert(JSON.stringify(err));
       });
 
     //request to get all the attendees for this event
-    requestAPI({ url: "http://localhost:8080/api/event/jointUsers/" + this.eventid ,
-      method: "POST",
-      headers:{
-        'Content-Type':'application/json'
-      }})
-            .then(res => {
-              this.enrollment = JSON.parse(res);
-            })
-            .catch(err => {
-              // alert(JSON.stringify(err));
-            });
+    requestAPI({
+      url: "http://localhost:8080/api/event/eventJointUsers/" + this.eventid,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        this.enrollment = JSON.parse(JSON.stringify(res)).data;
+      })
+      .catch(err => {
+        alert(JSON.stringify(err));
+      });
   },
 
   data() {
     return {
       like: false,
       isDisabled: true,
-      enrollment:"",
-      img: require("@/assets/img/faces/kendall.jpg"),
+      enrollment: "",
       event: [],
       carousel: [
         {
@@ -250,7 +284,7 @@ export default {
       this.$router.push(NewPage);
       this.$router.go(-1);
     },
-    uploadImgSuccess(res, file) {
+    uploadImgSuccess() {
       console.log("uploadImgSuccess");
       this.$notify.success();
       //在此处刷新当前界面
@@ -258,7 +292,7 @@ export default {
       this.$router.push(NewPage);
       this.$router.go(-1);
     },
-    beforImgUpload(file) {
+    beforImgUpload() {
       console.log("beforImgUpload");
     },
     submitUpload() {
@@ -269,8 +303,8 @@ export default {
       this.requestAPI({
         url: " http://localhost:8080/api/user/image/" + this.eventid,
         method: "GET",
-        headers:{
-          'Content-Type':'application/json'
+        headers: {
+          "Content-Type": "application/json"
         },
         body: this.eventid
       })
@@ -282,43 +316,17 @@ export default {
         });
     },
     //like request, add/remove record from DB
-    clickLike(){
-      if(this.like)
-        this.like = false;
-      else
-        this.like  = true;
-      // requestAPI({
-      //   url: "http://localhost:8080/api/user/favorateEvent/",
-      //   method: "POST",
-      //   headers:{
-      //     'Content-Type':'application/json'
-      //   },
-      //   body: {
-      //     userId: 12,
-      //     eventId: 21
-      //   }
-      // })
-      //         .then(res => {
-      //           alert(JSON.stringify(this.userInfo) + " success "+JSON.stringify(res));
-      //           console.log(res);
-      //         })
-      //         .catch(err => {
-      //           alert(JSON.stringify(this.userInfo) + " error "+JSON.stringify(err));
-      //           console.log(err);
-      //         });
-
-    },
-    //participant request, add or remove record from DB
-    participant(){
+    clickLike() {
+      this.like = !this.like;
       requestAPI({
-        url: "http::localhost:8080/api/user/jointEvent/",
+        url: "http://localhost:8080/api/user/favorateEvent/",
         method: "POST",
         headers:{
           'Content-Type':'application/json'
         },
         body: {
-          userId: this.userInfo.userId,
-          eventId: this.eventid
+          userId: JSON.parse(localStorage.getItem("Authorization")).id,
+          eventId: this.$route.params.eventid
         }
       })
               .then(res => {
@@ -326,9 +334,35 @@ export default {
                 console.log(res);
               })
               .catch(err => {
-                alert(JSON.stringify(this.userInfo) + " error "+JSON.stringify(err));
+                alert(JSON.stringify(this.userInfo) + " "+ this.eventId +" error "+JSON.stringify(err));
                 console.log(err);
               });
+    },
+    //participant request, add or remove record from DB
+    participant() {
+      requestAPI({
+        url: "http::localhost:8080/api/user/jointEvent/",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: {
+          userId: this.userInfo.userId,
+          eventId: this.eventid
+        }
+      })
+        .then(res => {
+          alert(
+            JSON.stringify(this.userInfo) + " success " + JSON.stringify(res)
+          );
+          console.log(res);
+        })
+        .catch(err => {
+          alert(
+            JSON.stringify(this.userInfo) + " error " + JSON.stringify(err)
+          );
+          console.log(err);
+        });
     }
   }
 };
