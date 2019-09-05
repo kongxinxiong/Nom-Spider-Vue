8d2173c05723620f34876bedaeea28e10525822c
Meihan <330556078@qq.com>
2019-09-05 19:43:13 +0800

    add notification

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -42,13 +42,38 @@
                 <p>Profile</p>
               </md-list-item>
 
-              <md-list-item href="#/profile">
-                <i class="material-icons">notifications</i>
-                <p>
-                  Notification
-                </p>
-                <md-badge md-content="1"></md-badge>
-              </md-list-item>
+              <li class="md-list-item">
+                <a
+                  href="#"
+                  class="md-list-item-router md-list-item-container md-button-clean dropdown"
+                  @click="readNotification"
+                >
+                  <div class="md-list-item-content">
+                    <drop-down direction="down">
+                      <md-button
+                        slot="title"
+                        class="md-button md-button-link md-simple dropdown-toggle"
+                        data-toggle="dropdown"
+                      >
+                        <i class="material-icons">notifications</i>
+                        <p>Notification</p>
+                        <md-badge
+                          :md-content="notificationNum"
+                          v-if="hasNotification"
+                        ></md-badge>
+                      </md-button>
+                      <ul class="dropdown-menu dropdown-with-icons">
+                        <li v-for="event in events">
+                          <a @click="go4Details(event.id)">
+                            <i class="material-icons">access_time</i>
+                            <p><strong>{{event.startdate}} {{event.title}}</strong></p>
+                          </a>
+                        </li>
+                      </ul>
+                    </drop-down>
+                  </div>
+                </a>
+              </li>
 
               <li class="md-list-item" v-if="!isLogin">
                 <a
@@ -94,6 +119,7 @@
 
 <script>
 let resizeTimeout;
+import requestAPI from "../plugins/request";
 function resizeThrottler(actualResizeHandler) {
   // ignore resize events as long as an actualResizeHandler execution is in the queue
   if (!resizeTimeout) {
@@ -138,26 +164,38 @@ export default {
       default: require("@/assets/img/faces/christian.jpg")
     }
   },
-  created(){
-    this.isLogin=(localStorage.getItem('Authorization')!=null);
-    //alert(this.isLogin);
+  created() {
+    this.userInfo = JSON.parse(localStorage.getItem("Authorization"));
+    this.isLogin = localStorage.getItem("Authorization") != null;
+    this.getNotification(this.userInfo.id);
+    localStorage.setItem("readNotification", "false");
   },
   data() {
     return {
       extraNavClasses: "",
       toggledClass: false,
-      isLogin:false
+      isLogin: false,
+      notificationNum: 11,
+      events: [
+        {
+          title: "Hiking Saturday",
+          startdate: "2019-09-09",
+          id:1
+        },
+        {
+          title: "Kakaro Night",
+          startdate: "2019-09-15",
+          id:2
+        }
+      ],
+      read: false
     };
   },
   computed: {
-    showDownload() {
-      const excludedRoutes = ["login", "landing", "profile"];
-      return excludedRoutes.every(r => r !== this.$route.name);
-    },
     hasNotification() {
-      let notificationNum = 1;
-      return notificationNum > 0;
-    },
+      console.log(this.read);
+      return this.notificationNum > 0 && !(this.read === "true");
+    }
   },
   methods: {
     bodyClick() {
@@ -203,6 +241,34 @@ export default {
       if (element_id) {
         element_id.scrollIntoView({ block: "end", behavior: "smooth" });
       }
+    },
+    getNotification(userId) {
+      requestAPI({
+        url:
+          "http://localhost:8080/api//user/userJoint7DaysComingEvents/" +
+          userId,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => {
+          this.events = JSON.parse(JSON.stringify(res)).data;
+          this.notificationNum = this.events.length;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    readNotification() {
+      localStorage.setItem("readNotification", "true");
+      this.read = localStorage.getItem("readNotification");
+    },
+    go4Details(id) {
+      this.$router.push({
+        name: "eventInfo",
+        params: {eventid: id}
+      });
     }
   },
   mounted() {
@@ -230,5 +296,58 @@ export default {
   width: 18px;
   height: 18px;
   margin-left: 5px;
+}
+.md-button.md-simple i,
+.md-button.md-simple:hover i,
+.md-button.md-simple:focus i,
+.md-button.md-simple:active i,
+.md-button.md-simple.active i,
+.md-button.md-simple:active:focus i,
+.md-button.md-simple:active:hover i,
+.md-button.md-simple.active:focus i,
+.md-button.md-simple.active:hover i,
+.open > .md-button.md-simple.dropdown-toggle i,
+.open > .md-button.md-simple.dropdown-toggle:focus i,
+.open > .md-button.md-simple.dropdown-toggle:hover i,
+.md-button.md-default.md-simple i,
+.md-button.md-default.md-simple:hover i,
+.md-button.md-default.md-simple:focus i,
+.md-button.md-default.md-simple:active i,
+.md-button.md-default.md-simple.active i,
+.md-button.md-default.md-simple:active:focus i,
+.md-button.md-default.md-simple:active:hover i,
+.md-button.md-default.md-simple.active:focus i,
+.md-button.md-default.md-simple.active:hover i,
+.open > .md-button.md-default.md-simple.dropdown-toggle i,
+.open > .md-button.md-default.md-simple.dropdown-toggle:focus i,
+.open > .md-button.md-default.md-simple.dropdown-toggle:hover i {
+  color: #ffffff !important;
+}
+
+.md-button.md-simple,
+.md-button.md-simple:hover,
+.md-button.md-simple:focus,
+.md-button.md-simple:active,
+.md-button.md-simple.active,
+.md-button.md-simple:active:focus,
+.md-button.md-simple:active:hover,
+.md-button.md-simple.active:focus,
+.md-button.md-simple.active:hover,
+.open > .md-button.md-simple.dropdown-toggle,
+.open > .md-button.md-simple.dropdown-toggle:focus,
+.open > .md-button.md-simple.dropdown-toggle:hover,
+.md-button.md-default.md-simple,
+.md-button.md-default.md-simple:hover,
+.md-button.md-default.md-simple:focus,
+.md-button.md-default.md-simple:active,
+.md-button.md-default.md-simple.active,
+.md-button.md-default.md-simple:active:focus,
+.md-button.md-default.md-simple:active:hover,
+.md-button.md-default.md-simple.active:focus,
+.md-button.md-default.md-simple.active:hover,
+.open > .md-button.md-default.md-simple.dropdown-toggle,
+.open > .md-button.md-default.md-simple.dropdown-toggle:focus,
+.open > .md-button.md-default.md-simple.dropdown-toggle:hover {
+  color: #ffffff !important;
 }
 </style>
